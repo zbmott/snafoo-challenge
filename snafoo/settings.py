fdf2d020f7c68c1e343491425c4e5c83eef6aab7
Zachary Mott <zach.mott@gmail.com>
2018-05-06 00:26:11 -0500

    Fix django.contrib.staticfiles configuration.

--- snafoo/settings.py
+++ snafoo/settings.py
@@ -115,9 +115,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # +------------------------------------------------------------------------------------------------+
 # |                                                                                                |
