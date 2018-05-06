e043c6cf8f5ebc2a681a211ef26051090c2de440
Zachary Mott <zach.mott@gmail.com>
2018-05-06 00:51:25 -0500

    Add Ansible provisioning playbook.

--- snafoo/settings.py
+++ snafoo/settings.py
@@ -82,15 +82,15 @@ DATABASES = {}  # Define me in local_settings.py.
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
-#    {
-#        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-#    },
-#    {
-#        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-#    },
-#    {
-#        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-#    },
+   {
+       'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+   },
+   {
+       'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+   },
+   {
+       'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+   },
     {
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
